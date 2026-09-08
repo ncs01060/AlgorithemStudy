1745b4317abf6d4064567f2abda10b6e35c127e0
ncs01060 <ncs01045@gmail.com>
2026-09-08 10:53:26 +0900

    [DFS/BFS] 소수찾기 solve

---
+++ 프로그래머스/DFS_BFS/소수_찾기.py
@@ -0,0 +1,25 @@
+def check(numbers,current,visited,prime:set):
+    for i in range(0,len(numbers)):
+        if visited[i]:
+            continue
+        else:
+            visited[i] = True
+            current+=numbers[i]
+            if int(current) > 1:
+                for j in range(2,int(current)):
+                    if int(current) % j == 0:
+                        break
+                else:
+                    prime.add(int(current))
+            check(numbers,current,visited,prime)
+            visited[i] = False
+            current = current[:-1]     
+    return prime
+    
+
+def solution(numbers):
+    prime = set()
+    visited = [False] * len(numbers)
+    answer = check(numbers, "",visited,prime)
+    return len(answer)
+print(solution("17"))
