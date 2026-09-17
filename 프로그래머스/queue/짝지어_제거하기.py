e720b1af1385f723b930cce25012f0d392842af6
ncs01060 <ncs01045@gmail.com>
2026-09-17 14:39:16 +0900

    [Stack] 짝지어 제거하기 solve

---
+++ 프로그래머스/queue/짝지어_제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    stack = [s[0]]
+    
+    for i in range(1,len(s)):
+        if len(stack) != 0 and stack[-1] == s[i]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+    
+    if len(stack) == 0:
+        answer = 1
+    
+    return answer
